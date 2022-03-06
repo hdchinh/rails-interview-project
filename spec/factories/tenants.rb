f067202dae22ada8965ca204c5251577e8bfb300
Duy Chinh <hduychinh@gmail.com>
2022-03-06 11:36:26 +0700

    travis setup

--- spec/factories/tenants.rb
+++ spec/factories/tenants.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :tenant do
-    name { Faker::Company.name }
+    name { "#{Faker::Company.name}-#{SecureRandom.hex(16).last(5)}" }
     access_count { 0 }
   end
 end
